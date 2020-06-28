961ccab2fb62eeca16e55052b46ae20a845ab825
Bernardo Goes <bernardogoes@gmail.com>
2020-06-28 17:29:05 +0100

    Restore navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true, format: { with: /\A.*@.*\.com\z/ }
+  validates :first_name, presence: true, uniqueness: { scope: :last_name }
+  validates :last_name, presence: true
+  validates :password, length: { in: 6..10 }
 end
