d0db4a5e9a706b3bece592769792b503b1dd537f
Bernardo Goes <bernardogoes@gmail.com>
2020-06-28 18:35:46 +0100

    Menu-bar beginning to work

---
+++ app/javascript/components/menu-bar.js
@@ -0,0 +1,8 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+
+const openNav = () => {
+  document.getElementById("mySidebar").classList.toggle("toggle-menu-bar") ;
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+export { openNav };
